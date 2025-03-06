6dda14a356a4b30720336da64e662f1984b76a9f
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-03-06 19:20:18 -0300

    Create ExercicioModelo.cs

---
+++ aula01/ExercicioModelo.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Digite a sua idade:");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if(idade >= 18) {
+    Console.WriteLine("Maior");
+} else {
+    Console.WriteLine("Menor");
+}
