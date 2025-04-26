7b7e68cd3fdd69e56aaf3fc80018b54d365566b7
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-04-26 11:45:39 -0300

    Create ParImar.cs

---
+++ aula05/ParImar.cs
@@ -0,0 +1,10 @@
+// 3 - Par ou ímpar
+
+Console.WriteLine("Digite o primeiro número: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0) {
+    Console.WriteLine(num . "é par.");
+} else {
+    Console.WriteLine(num . "é ímpar.");
+}
