7e1f68e4dc36edb2ac3eaa4e6779089b85126c73
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-04-26 11:43:57 -0300

    Create Soma.cs

---
+++ aula05/Soma.cs
@@ -0,0 +1,9 @@
+// 1 - Soma de dois números
+
+Console.WriteLine("Digite o primeiro número: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o segundo número:");
+int num2 = int.Parse(Console.ReadLine());
+
+int soma = num1 + num2;
+Console.WriteLine(soma);
