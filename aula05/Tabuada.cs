ddb5d779e1b49c59149dcca9622a72c3dbe13229
Rafael Sancor <rafaelsancor2003@hotmail.com>
2025-04-26 11:46:12 -0300

    Create Tabuada.cs

---
+++ aula05/Tabuada.cs
@@ -0,0 +1,8 @@
+Console.WriteLine("Digite o primeiro número: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = ; i < 11; i++) {
+    int tabuada = i * num;
+    Console.WriteLine("Tabuada do " . num);
+    Console.WriteLine(num " * " i " = " . tabuada);
+}
